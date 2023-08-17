d8b7e5d59edfb09145406d4fdc42d28538fe9efb
liamjdavis <66973153+liamjdavis@users.noreply.github.com>
2023-08-17 10:22:46 -0700

    Add files via upload

--- vote counter.py
+++ vote counter.py
@@ -1,12 +1,14 @@
 import names
 import random
 
-class Ballot():
+
+class Ballot:
     def __init__(self, firstChoice, secondChoice, thirdChoice, fourthChoice):
         self.firstChoice = firstChoice
         self.secondChoice = secondChoice
-        self.thirdCHoice = thirdChoice
+        self.thirdChoice = thirdChoice
         self.fourthChoice = fourthChoice
+
 
 def main():
     candidates = createCandidates()
@@ -14,7 +16,6 @@ def main():
 
     # sort ballots by first choice vote
     sortedBallots = sortBallots(ballots, candidates)
-    print(sortedBallots)
     # eliminate candidate with least first choice votes
     # add second choice votes to other candidates
 
@@ -22,32 +23,35 @@ def main():
 
     # eliminate the candidate with the least first choice votes
 
-    # assign votes of eliminated candidates to highest ranked remaining candidate
+    # assign votes of eliminated candidates to the highest ranked remaining candidate
+
 
 def createCandidates():
     candidates = []
 
     # assign random name to 4 candidates
-    for i in range (4):
+    for i in range(4):
         name = names.get_full_name
         candidates.append(name)
 
     return candidates
 
+
 def createBallots(numBallots, candidates):
     ballots = []
 
     # create four random unique votes
-    for i in range (numBallots):
+    for i in range(numBallots):
         randInt1 = getRandInt(0, len(candidates), -1, -1, -1)
         randInt2 = getRandInt(0, len(candidates), randInt1, -1, -1)
         randInt3 = getRandInt(0, len(candidates), randInt1, randInt2, -1)
-        randInt4 = getRandInt(0, len(candidates), randInt1, randInt2, randInt3, randInt4)
+        randInt4 = getRandInt(0, len(candidates), randInt1, randInt2, randInt3)
 
         ballot = Ballot(candidates[randInt1], candidates[randInt2], candidates[randInt3], candidates[randInt4])
         ballots.append(ballot)
-    
+
     return ballots
+
 
 def sortBallots(ballots, candidates):
     sortedBallots = {}
@@ -55,9 +59,10 @@ def sortBallots(ballots, candidates):
     for ballot in ballots:
         firstChoice = ballot.firstChoice
         index_toAppend = candidates.index(firstChoice in candidates)
-        sortBallots[index_toAppend].append(ballot)
+        sortedBallots[index_toAppend].insert(0, ballot)
 
     return sortedBallots
+
 
 def getRandInt(start, end, exclude1, exclude2, exclude3):
     while True:
